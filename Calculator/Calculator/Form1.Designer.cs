7f910b2614ae61d1cf439c95335cb6eaf76fda54
TimWCA <vihrianov1@outlook.com>
2020-02-10 15:35:07 +0700

    CS-Calculator Version 1.0 It can compute basic math operations

---
+++ Calculator/Calculator/Form1.Designer.cs
@@ -0,0 +1,242 @@
+﻿namespace Calculator
+{
+    partial class CalculatorWindow
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(CalculatorWindow));
+            this.mathText = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.equalButton = new System.Windows.Forms.Button();
+            this.plusButton = new System.Windows.Forms.Button();
+            this.minusButton = new System.Windows.Forms.Button();
+            this.mulButton = new System.Windows.Forms.Button();
+            this.delButton = new System.Windows.Forms.Button();
+            this.button0 = new System.Windows.Forms.Button();
+            this.clearButton = new System.Windows.Forms.Button();
+            this.dotButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // mathText
+            // 
+            resources.ApplyResources(this.mathText, "mathText");
+            this.mathText.Name = "mathText";
+            // 
+            // button1
+            // 
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            resources.ApplyResources(this.button1, "button1");
+            this.button1.Name = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Cursor = System.Windows.Forms.Cursors.Hand;
+            resources.ApplyResources(this.button2, "button2");
+            this.button2.Name = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Cursor = System.Windows.Forms.Cursors.Hand;
+            resources.ApplyResources(this.button3, "button3");
+            this.button3.Name = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Cursor = System.Windows.Forms.Cursors.Hand;
+            resources.ApplyResources(this.button4, "button4");
+            this.button4.Name = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Cursor = System.Windows.Forms.Cursors.Hand;
+            resources.ApplyResources(this.button5, "button5");
+            this.button5.Name = "button5";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Cursor = System.Windows.Forms.Cursors.Hand;
+            resources.ApplyResources(this.button6, "button6");
+            this.button6.Name = "button6";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Cursor = System.Windows.Forms.Cursors.Hand;
+            resources.ApplyResources(this.button7, "button7");
+            this.button7.Name = "button7";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Cursor = System.Windows.Forms.Cursors.Hand;
+            resources.ApplyResources(this.button8, "button8");
+            this.button8.Name = "button8";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.Cursor = System.Windows.Forms.Cursors.Hand;
+            resources.ApplyResources(this.button9, "button9");
+            this.button9.Name = "button9";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // equalButton
+            // 
+            this.equalButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            resources.ApplyResources(this.equalButton, "equalButton");
+            this.equalButton.Name = "equalButton";
+            this.equalButton.UseVisualStyleBackColor = true;
+            this.equalButton.Click += new System.EventHandler(this.equalButton_Click);
+            // 
+            // plusButton
+            // 
+            resources.ApplyResources(this.plusButton, "plusButton");
+            this.plusButton.Name = "plusButton";
+            this.plusButton.UseVisualStyleBackColor = true;
+            this.plusButton.Click += new System.EventHandler(this.plusButton_Click);
+            // 
+            // minusButton
+            // 
+            resources.ApplyResources(this.minusButton, "minusButton");
+            this.minusButton.Name = "minusButton";
+            this.minusButton.UseVisualStyleBackColor = true;
+            this.minusButton.Click += new System.EventHandler(this.minusButton_Click);
+            // 
+            // mulButton
+            // 
+            resources.ApplyResources(this.mulButton, "mulButton");
+            this.mulButton.Name = "mulButton";
+            this.mulButton.UseVisualStyleBackColor = true;
+            this.mulButton.Click += new System.EventHandler(this.mulButton_Click);
+            // 
+            // delButton
+            // 
+            resources.ApplyResources(this.delButton, "delButton");
+            this.delButton.Name = "delButton";
+            this.delButton.UseVisualStyleBackColor = true;
+            this.delButton.Click += new System.EventHandler(this.delButton_Click);
+            // 
+            // button0
+            // 
+            this.button0.Cursor = System.Windows.Forms.Cursors.Hand;
+            resources.ApplyResources(this.button0, "button0");
+            this.button0.Name = "button0";
+            this.button0.UseVisualStyleBackColor = true;
+            this.button0.Click += new System.EventHandler(this.button0_Click);
+            // 
+            // clearButton
+            // 
+            resources.ApplyResources(this.clearButton, "clearButton");
+            this.clearButton.Name = "clearButton";
+            this.clearButton.UseVisualStyleBackColor = true;
+            this.clearButton.Click += new System.EventHandler(this.clearButton_Click);
+            // 
+            // dotButton
+            // 
+            this.dotButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            resources.ApplyResources(this.dotButton, "dotButton");
+            this.dotButton.Name = "dotButton";
+            this.dotButton.UseVisualStyleBackColor = true;
+            this.dotButton.Click += new System.EventHandler(this.dotButton_Click);
+            // 
+            // CalculatorWindow
+            // 
+            this.AllowDrop = true;
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CausesValidation = false;
+            this.Controls.Add(this.dotButton);
+            this.Controls.Add(this.clearButton);
+            this.Controls.Add(this.button0);
+            this.Controls.Add(this.delButton);
+            this.Controls.Add(this.mulButton);
+            this.Controls.Add(this.minusButton);
+            this.Controls.Add(this.plusButton);
+            this.Controls.Add(this.equalButton);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.mathText);
+            this.MaximizeBox = false;
+            this.Name = "CalculatorWindow";
+            this.Load += new System.EventHandler(this.CalculatorWindow_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox mathText;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button equalButton;
+        private System.Windows.Forms.Button plusButton;
+        private System.Windows.Forms.Button minusButton;
+        private System.Windows.Forms.Button mulButton;
+        private System.Windows.Forms.Button delButton;
+        private System.Windows.Forms.Button button0;
+        private System.Windows.Forms.Button clearButton;
+        private System.Windows.Forms.Button dotButton;
+    }
+}
+
---
+++ Calculator/Calculator/Form1.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculator
+{
+    public partial class CalculatorWindow : Form
+    {
+
+        public CalculatorWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void CalculatorWindow_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        //
+        //Цифровая клавиатура
+        //
+
+        private void button1_Click(object sender, EventArgs e) //Обработка нажатия кнопки -1-
+        {
+            mathText.Text += "1";
+        }
+
+        private void button2_Click(object sender, EventArgs e) //Обработка нажатия кнопки -2-
+        {
+            mathText.Text += "2";
+        }
+
+        private void button3_Click(object sender, EventArgs e) //Обработка нажатия кнопки -3-
+        {
+            mathText.Text += "3";
+        }
+
+        private void button4_Click(object sender, EventArgs e) //Обработка нажатия кнопки -4-
+        {
+            mathText.Text += "4";
+        }
+
+        private void button5_Click(object sender, EventArgs e) //Обработка нажатия кнопки -5-
+        {
+            mathText.Text += "5";
+        }
+
+        private void button6_Click(object sender, EventArgs e) //Обработка нажатия кнопки -6-
+        {
+            mathText.Text += "6";
+        }
+
+        private void button7_Click(object sender, EventArgs e) //Обработка нажатия кнопки -7-
+        {
+            mathText.Text += "7";
+        }
+
+        private void button8_Click(object sender, EventArgs e) //Обработка нажатия кнопки -8-
+        {
+            mathText.Text += "8";
+        }
+
+        private void button9_Click(object sender, EventArgs e) //Обработка нажатия кнопки -9-
+        {
+            mathText.Text += "9";
+        }
+
+        private void button0_Click(object sender, EventArgs e) //Обработка нажатия кнопки -0-
+        {
+            mathText.Text += "0";
+        }
+
+        //
+        //Клавиши действий
+        //
+
+        private void clearButton_Click(object sender, EventArgs e) //Обработка нажатия кнопки -C-
+        {
+            mathText.Text = "";
+        }
+
+        private void plusButton_Click(object sender, EventArgs e) //Обработка нажатия кнопки -+-
+        {
+            mathText.Text += "+";
+        }
+
+        private void minusButton_Click(object sender, EventArgs e) //Обработка нажатия кнопки ---
+        {
+            mathText.Text += "-";
+        }
+
+        private void mulButton_Click(object sender, EventArgs e) //Обработка нажатия кнопки -*-
+        {
+            mathText.Text += "*";
+        }
+
+        private void delButton_Click(object sender, EventArgs e) //Обработка нажатия кнопки -/-
+        {
+            mathText.Text += "/";
+        }
+
+        private void dotButton_Click(object sender, EventArgs e) //Обработка нажатия кнопки -.-
+        {
+            mathText.Text += ".";
+        }
+
+        //
+        //Вычисление действия
+        //
+
+        private void equalButton_Click(object sender, EventArgs e) //Обработка нажатия кнопки -=-
+        {
+            System.Threading.Thread.CurrentThread.CurrentCulture = new System.Globalization.CultureInfo("en-US");
+            var ans = new DataTable().Compute(mathText.Text, null);
+            mathText.Text = "" + ans;
+            
+        }
+    }
+}
